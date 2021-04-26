50d067e85b2b3985c3420ca1d4f79f592446ff23
chase-abbott <cabbott93@gmail.com>
2021-04-26 13:27:23 -0700

    Create header and footer

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="Footer"> 
+            The best creatures in the world!
+      </footer>
+    );
+  }
+}
+
+export default Footer;
