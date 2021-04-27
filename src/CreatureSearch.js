03ce04e8d00067e67739c09a3a142de7d21c2185
chase-abbott <cabbott93@gmail.com>
2021-04-27 13:12:38 -0700

    maintain state in search form

--- src/CreatureSearch.js
+++ src/CreatureSearch.js
@@ -2,14 +2,41 @@ import { Component } from 'react';
 import './CreatureSearch.css';
 
 export default class CreatureSearch extends Component {
-  
+  state = {
+    nameFilter: '',
+    sortField : '',
+  }
+
+  handleNamerFilter = ({ target }) => {
+    this.setState({ nameFilter : target.value });
+  }
+
+  handleSortField = ({ target }) => {
+    this.setState({ sortField : target.value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    
+  }
+
   render() {
+    const { nameFilter, sortField } = this.state;
     return (
-      <form className="CreatureSearch">
+      <form 
+        className="CreatureSearch"
+        onSubmit={this.handleSubmit}
+      >
 
-        <input></input>
+        <input
+          value={nameFilter}
+          onChange={this.handleNamerFilter}
+        />
 
-        <select>
+        <select 
+          value={sortField}
+          onChange={this.handleSortField}
+        >
           <option value=""> sort.. </option>
           <option value="name"> name </option>
           <option value="horns"> horns </option>
